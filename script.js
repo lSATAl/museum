980614415c7dba7b548e475504c1a5c03d4500a9
lSATAl <70383465+lSATAl@users.noreply.github.com>
2021-09-27 12:47:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const progress = document.querySelector('.video_progress');
+const vol = document.querySelector('.video_vol');
+
+progress.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
+vol.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
+
+
